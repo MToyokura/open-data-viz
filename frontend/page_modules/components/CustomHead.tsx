ce728323854de287bf796ae2bf7671697e009aa9
MToyokura <mikitoyo6@gmail.com>
2023-03-02 22:40:31 +0900

    Fix "No Script Tags In Head Component" error

--- frontend/page_modules/components/CustomHead.tsx
+++ frontend/page_modules/components/CustomHead.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Script from "next/script";
 import { appName } from "../../pages/_app";
 
 export const CustomHead = (props: { pageTitle?: string }) => {
@@ -10,11 +11,11 @@ export const CustomHead = (props: { pageTitle?: string }) => {
       ) : (
         <title>{appName}</title>
       )}
-      <script
+      <Script
         defer
         data-domain="mtoyokura.github.io"
         src="https://plausible.io/js/script.js"
-      ></script>
+      />
     </Head>
   );
 };
